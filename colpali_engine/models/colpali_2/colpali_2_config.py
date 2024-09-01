0edd761476b28e6bb6c2dc3f79804b6375a8ae3c
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-01 17:50:26 +0200

    feat: replace CLS token with pooled embedding

--- colpali_engine/models/colpali_2/colpali_2_config.py
+++ colpali_engine/models/colpali_2/colpali_2_config.py
@@ -7,10 +7,12 @@ class ColPali2Config(PretrainedConfig):
         self,
         vlm_config: PaliGemmaConfig,
         single_vector_projector_dim: int = 128,
+        single_vector_pool_strategy: str = "mean",
         multi_vector_projector_dim: int = 128,
         **kwargs,
     ):
         super().__init__(**kwargs)
         self.vlm_config = vlm_config
         self.single_vector_projector_dim = single_vector_projector_dim
+        self.single_vector_pool_strategy = single_vector_pool_strategy
         self.multi_vector_projector_dim = multi_vector_projector_dim
