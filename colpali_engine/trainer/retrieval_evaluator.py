46c43c6a06778b191c512aed9854d5324153c25a
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-06 17:31:07 +0200

    feat: tweak `get_torch_device`

--- colpali_engine/trainer/retrieval_evaluator.py
+++ colpali_engine/trainer/retrieval_evaluator.py
@@ -3,18 +3,7 @@ from typing import Dict, List
 import torch
 from mteb.evaluation.evaluators import RetrievalEvaluator
 
-
-def get_torch_device() -> str:
-    """
-    Returns the device and dtype to be used for torch tensors.
-    """
-    if torch.cuda.is_available():
-        device = "cuda:0"
-    elif torch.backends.mps.is_available():  # for Apple Silicon
-        device = "mps"
-    else:
-        device = "cpu"
-    return device
+from colpali_engine.utils.torch_utils import get_torch_device
 
 
 class CustomRetrievalEvaluator:
--- colpali_engine/utils/torch_utils.py
+++ colpali_engine/utils/torch_utils.py
@@ -1,14 +1,22 @@
 import torch
 
 
-def get_torch_device() -> str:
+def get_torch_device(device: str = "auto") -> str:
     """
-    Returns the device and dtype to be used for torch tensors.
+    Returns the device (string) to be used by PyTorch.
+
+    `device` arg defaults to "auto" which will use:
+    - "cuda:0" if available
+    - else "mps" if available
+    - else "cpu".
     """
-    if torch.cuda.is_available():
-        device = "cuda:0"
-    elif torch.backends.mps.is_available():  # for Apple Silicon
-        device = "mps"
+
+    if device == "auto":
+        if torch.cuda.is_available():
+            return "cuda:0"
+        elif torch.backends.mps.is_available():  # for Apple Silicon
+            return "mps"
+        else:
+            return "cpu"
     else:
-        device = "cpu"
-    return device
+        return device
