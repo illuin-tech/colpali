c123156e0f87b2dacbed4cddcb0fb01b3cbee821
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-07 10:49:26 +0200

    feat: use PaliGemma tokenizer in `process_queries`

--- colpali_engine/models/late_interaction/colpali/processing_colpali.py
+++ colpali_engine/models/late_interaction/colpali/processing_colpali.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import List, Optional, cast
 
 from PIL import Image
-from transformers import BatchFeature, PaliGemmaProcessor
+from transformers import BatchEncoding, BatchFeature, PaliGemmaProcessor
 
 from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
 
@@ -22,7 +22,7 @@ class ColPaliProcessor(BaseVisualRetrieverProcessor, PaliGemmaProcessor):
         images: List[Image.Image],
     ) -> BatchFeature:
         """
-        Process images for ColPali, with an efficient tweak around the PaliGemmma processor.
+        Process images for ColPali.
         """
         texts_doc = ["Describe the image."] * len(images)
         images = [image.convert("RGB") for image in images]
@@ -41,13 +41,13 @@ class ColPaliProcessor(BaseVisualRetrieverProcessor, PaliGemmaProcessor):
         queries: List[str],
         max_length: int = 50,
         suffix: Optional[str] = None,
-    ) -> BatchFeature:
+    ) -> BatchEncoding:
         """
-        Process queries for ColPali, with an efficient tweak around the PaliGemmma processor.
-        """
-        # NOTE: The image is required for calling PaligemmaProcessor, so we create a mock image here.
-        mock_image = Image.new("RGB", (448, 448), (255, 255, 255)).convert("RGB")
+        Process queries for ColPali.
 
+        NOTE: This method uses the PaliGemma tokenizer instead of the processor because
+        the processor cannot handle text without images.
+        """
         suffix = suffix or "<pad>" * 10
         texts_query: List[str] = []
 
@@ -56,18 +56,11 @@ class ColPaliProcessor(BaseVisualRetrieverProcessor, PaliGemmaProcessor):
             query += suffix  # add suffix (pad tokens)
             texts_query.append(query)
 
-        batch_query = self(
-            images=[mock_image] * len(texts_query),
-            text=texts_query,
+        batch_query = self.tokenizer(
+            texts_query,
             return_tensors="pt",
             padding="longest",
-            max_length=max_length + self.image_seq_length,
+            max_length=max_length,
         )
-        del batch_query["pixel_values"]
-
-        # TODO: call the tokenizer instead?
-
-        batch_query["input_ids"] = batch_query["input_ids"][..., self.image_seq_length :]
-        batch_query["attention_mask"] = batch_query["attention_mask"][..., self.image_seq_length :]
 
         return batch_query
