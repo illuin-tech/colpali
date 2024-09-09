9ea85dc8bc274d8b9c68c8927821beaf6f038589
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-09 16:00:26 +0200

    feat: inherit processor directly in ColIdefics2Processor

--- colpali_engine/models/idefics_2/colidefics_2/processing_colidefics_2.py
+++ colpali_engine/models/idefics_2/colidefics_2/processing_colidefics_2.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Optional, cast
+from typing import List, Optional
 
 from PIL import Image
 from transformers import BatchEncoding, Idefics2Processor
@@ -8,26 +8,22 @@ from transformers import BatchEncoding, Idefics2Processor
 from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
 
 
-class ColIdefics2Processor(BaseVisualRetrieverProcessor):
+class ColIdefics2Processor(BaseVisualRetrieverProcessor, Idefics2Processor):
     """
     Processor for ColIdefics2.
     """
 
-    def __init__(
-        self,
-        pretrained_model_name_or_path: str = "HuggingFaceM4/idefics2-8b",
-    ):
-        super().__init__()
-        self.processor = cast(Idefics2Processor, Idefics2Processor.from_pretrained(pretrained_model_name_or_path))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def process_images(
         self,
         images: List[Image.Image],
     ) -> BatchEncoding:
         """
-        Process images for ColIdefics2, with an efficient tweak around the Idefics2 processor.
+        Process images for ColIdefics2.
         """
-        texts_doc = []
+        texts_doc: List[str] = []
         images = [image.convert("RGB") for image in images]
 
         for _ in images:
@@ -41,10 +37,10 @@ class ColIdefics2Processor(BaseVisualRetrieverProcessor):
                 },
             ]
 
-            text_doc = self.processor.apply_chat_template(messages_doc, add_generation_prompt=False)
+            text_doc = self.apply_chat_template(messages_doc, add_generation_prompt=False)
             texts_doc.append(text_doc.strip())
 
-        batch_doc = self.processor(
+        batch_doc = self(
             text=texts_doc,
             images=images,
             return_tensors="pt",
@@ -59,10 +55,11 @@ class ColIdefics2Processor(BaseVisualRetrieverProcessor):
         suffix: Optional[str] = None,
     ) -> BatchEncoding:
         """
-        Process queries for ColIdefics2, with an efficient tweak around the Idefics2 processor.
+        Process queries for ColIdefics2.
         """
         suffix = suffix or "<end_of_utterance>" * 5
-        texts_query = []
+        texts_query: List[str] = []
+
         for query in queries:
             messages_query = [
                 {
@@ -75,10 +72,10 @@ class ColIdefics2Processor(BaseVisualRetrieverProcessor):
                     ],
                 },
             ]
-            text_query = self.processor.apply_chat_template(messages_query, add_generation_prompt=False).strip()
+            text_query = self.apply_chat_template(messages_query, add_generation_prompt=False).strip()
             texts_query.append(text_query)
 
-        batch_query = self.processor(
+        batch_query = self(
             text=texts_query,
             return_tensors="pt",
             padding="longest",
