7ef02f0e4ef33650c51e8f8eb2e15e7f4c6165e4
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-09 11:29:13 +0200

    Revert "feat: use PaliGemma tokenizer in `process_queries`"
    This reverts commit 7ec95cb10429d65e67b3a9e17f2ab2de5a7159e7.

--- colpali_engine/models/late_interaction/colpali/processing_colpali.py
+++ colpali_engine/models/late_interaction/colpali/processing_colpali.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import List, Optional, cast
 
 from PIL import Image
-from transformers import BatchEncoding, BatchFeature, LlamaTokenizerFast, PaliGemmaProcessor
+from transformers import BatchFeature, PaliGemmaProcessor
 
 from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
 
@@ -29,14 +29,13 @@ class ColPaliProcessor(BaseVisualRetrieverProcessor):
                 token=hf_token,
             ),
         )
-        self.tokenizer = cast(LlamaTokenizerFast, self.processor.tokenizer)  # type: ignore
 
     def process_images(
         self,
         images: List[Image.Image],
     ) -> BatchFeature:
         """
-        Process images for ColPali.
+        Process images for ColPali, with an efficient tweak around the PaliGemmma processor.
         """
         texts_doc = ["Describe the image."] * len(images)
         images = [image.convert("RGB") for image in images]
@@ -54,13 +53,13 @@ class ColPaliProcessor(BaseVisualRetrieverProcessor):
         queries: List[str],
         max_length: int = 50,
         suffix: Optional[str] = None,
-    ) -> BatchEncoding:
+    ) -> BatchFeature:
         """
-        Process queries for ColPali.
+        Process queries for ColPali, with an efficient tweak around the PaliGemmma processor.
+        """
+        # NOTE: The image is required for calling PaligemmaProcessor, so we create a mock image here.
+        mock_image = Image.new("RGB", (448, 448), (255, 255, 255)).convert("RGB")
 
-        NOTE: This method uses the PaliGemma tokenizer instead of the processor because
-        the processor cannot handle text without images.
-        """
         suffix = suffix or "<pad>" * 10
         texts_query: List[str] = []
 
@@ -69,10 +68,16 @@ class ColPaliProcessor(BaseVisualRetrieverProcessor):
             query += suffix  # add suffix (pad tokens)
             texts_query.append(query)
 
-        batch_query = self.tokenizer(
-            texts_query,
+        batch_query = self.processor(
+            images=[mock_image] * len(texts_query),
+            text=texts_query,
             return_tensors="pt",
             padding="longest",
-            max_length=max_length,
+            max_length=max_length + self.processor.image_seq_length,
         )
+        del batch_query["pixel_values"]
+
+        batch_query["input_ids"] = batch_query["input_ids"][..., self.processor.image_seq_length :]
+        batch_query["attention_mask"] = batch_query["attention_mask"][..., self.processor.image_seq_length :]
+
         return batch_query
