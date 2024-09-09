1e4b3a428d487d66474dc03a34b5500a584aa058
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-09 16:37:57 +0200

    feat: move ColPali2 classes to match restructured package

--- colpali_engine/models/__init__.py
+++ colpali_engine/models/__init__.py
@@ -1,3 +1,4 @@
 from .idefics_2 import BiIdefics2, ColIdefics2, ColIdefics2Processor
 from .paligemma import BiPali, BiPaliProcessor, BiPaliProj, ColPali, ColPaliProcessor
+from .paligemma import  ColPali2, ColPali2Config, ColPali2ModelOutput, ColPali2Processor
 from .qwen2 import BiQwen2, BiQwen2Processor, ColQwen2, ColQwen2Processor
