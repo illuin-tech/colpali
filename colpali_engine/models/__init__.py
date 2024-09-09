cc2c645e18f85fed94a2e99dac95b181e2d505ea
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-09 14:08:14 +0200

    feat: sort models module by vlm backbone

--- colpali_engine/models/__init__.py
+++ colpali_engine/models/__init__.py
@@ -1,4 +1,2 @@
-from .biidefics import BiIdefics2
-from .bipali import BiPali, BiPaliProj
-from .colidefics import ColIdefics2, ColIdefics2Processor
-from .colpali import ColPali, ColPaliProcessor
+from .idefics import BiIdefics2, ColIdefics2, ColIdefics2Processor
+from .paligemma import BiPali, BiPaliProj, ColPali, ColPaliProcessor
---
+++ colpali_engine/models/idefics/__init__.py
@@ -0,0 +1,2 @@
+from .biidefics import BiIdefics2
+from .colidefics import ColIdefics2, ColIdefics2Processor
