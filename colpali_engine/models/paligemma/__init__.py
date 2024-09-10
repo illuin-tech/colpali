bbebb3b2b1b39bcb3d93363506888c9efdd56e28
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-10 17:18:16 +0200

    style: apply linter

--- colpali_engine/models/paligemma/__init__.py
+++ colpali_engine/models/paligemma/__init__.py
@@ -2,7 +2,7 @@ from .bipali import BiPali, BiPaliProcessor, BiPaliProj
 from .colpali import ColPali, ColPaliProcessor
 from .colpali_2 import (
     ColPali2,
-    ColPali2Processor,
-    ColPali2ModelOutput,
     ColPali2Config,
+    ColPali2ModelOutput,
+    ColPali2Processor,
 )
