55101dce15b200085f3afd1b42a78f93e2f30a7f
Akshay Ballal <arballal95@gmail.com>
2024-09-10 18:49:45 +0000

    remove phi

--- colpali_engine/models/__init__.py
+++ colpali_engine/models/__init__.py
@@ -1,2 +1,2 @@
 from .bi_encoders import BiIdefics2, BiPali, BiPaliProj
-from .late_interaction import ColIdefics2, ColPali, ColPhi3, ColQwen
+from .late_interaction import ColIdefics2, ColPali, ColQwen
--- colpali_engine/models/late_interaction/__init__.py
+++ colpali_engine/models/late_interaction/__init__.py
@@ -1,4 +1,3 @@
 from .colidefics_architecture import *  # noqa: F403
 from .colpali_architecture import *  # noqa: F403
-from .colphi_architecture import * # noqa: F403
-from .colqwen_architecture import * # noqa: F403
+from .colqwen_architecture import * 
