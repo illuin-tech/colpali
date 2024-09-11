ae4223e4dbf38b2b5a682cc063bf1fc1e7cd6d0b
ManuelFay <manuel.faysse@illuin.tech>
2024-09-11 22:34:39 +0200

    add: qwen2

---
+++ colpali_engine/models/qwen2/colqwen2/modeling_colqwen2.py
@@ -0,0 +1,38 @@
+from typing import ClassVar
+
+import torch
+from torch import nn
+from transformers.models.qwen2_vl import Qwen2VLConfig, Qwen2VLModel, Qwen2VLPreTrainedModel
+
+
+class ColQwen2(Qwen2VLPreTrainedModel):
+    """
+    ColQwen2 model implementation from the "ColPali: Efficient Document Retrieval with Vision Language Models" paper.
+    """
+
+    main_input_name: ClassVar[str] = "doc_input_ids"  # transformers-related
+
+    def __init__(self, config: Qwen2VLConfig):
+        super().__init__(config=config)
+
+        self.model = Qwen2VLModel(config=config)
+        # TODO:  verify weight tying
+        self.dim = 128
+        self.custom_text_proj = nn.Linear(self.model.config.text_config.hidden_size, self.dim)
+
+        self.post_init()
+
+    def forward(self, *args, **kwargs) -> torch.Tensor:
+        # Delete output_hidden_states from kwargs
+        kwargs.pop("output_hidden_states", None)
+
+        outputs = self.model(*args, output_hidden_states=True, **kwargs)  # (batch_size, sequence_length, hidden_size)
+        last_hidden_states = outputs.hidden_states[-1]  # (batch_size, sequence_length, hidden_size)
+        proj = self.custom_text_proj(last_hidden_states)  # (batch_size, sequence_length, dim)
+
+        # L2 normalization
+        proj = proj / proj.norm(dim=-1, keepdim=True)  # (batch_size, sequence_length, dim)
+
+        proj = proj * kwargs["attention_mask"].unsqueeze(-1)  # (batch_size, sequence_length, dim)
+
+        return proj
---
+++ colpali_engine/models/qwen2/colqwen2/processing_colqwen2.py
@@ -0,0 +1,79 @@
+from typing import List, Optional, Union
+
+import torch
+from PIL import Image
+from transformers import BatchFeature
+from transformers.models.qwen2_vl import Qwen2VLProcessor
+
+from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
+
+
+class ColQwen2Processor(BaseVisualRetrieverProcessor, Qwen2VLProcessor):
+    """
+    Processor for ColQwen2.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.min_pixels = 256 * 28 * 28
+        self.max_pixels = 256 * 28 * 28     # 1280
+
+    def process_images(
+        self,
+        images: List[Image.Image],
+    ) -> BatchFeature:
+        """
+        Process images for ColPali.
+        """
+        texts_doc = (["<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe the image.<|im_end|>\n"]
+                     * len(images))
+        images = [image.convert("RGB").resize((448, 448)) for image in images]
+
+        batch_doc = self(
+            text=texts_doc,
+            images=images,
+            padding="longest",
+            return_tensors="pt"
+        )
+
+        return batch_doc
+
+    def process_queries(
+        self,
+        queries: List[str],
+        max_length: int = 50,
+        suffix: Optional[str] = None,
+    ) -> BatchFeature:
+        """
+        Process queries for ColPali.
+        """
+        if suffix is None:
+            suffix = "<pad>" * 10
+        texts_query: List[str] = []
+
+        for query in queries:
+            query = f"Question: {query}"
+            query += suffix  # add suffix (pad tokens)
+            texts_query.append(query)
+
+        batch_query = self(
+            text=texts_query,
+            return_tensors="pt",
+            padding="longest",
+            # max_length=max_length + self.image_seq_length,
+        )
+
+        return batch_query
+
+    def score(
+        self,
+        qs: List[torch.Tensor],
+        ps: List[torch.Tensor],
+        device: Optional[Union[str, torch.device]] = None,
+        **kwargs,
+    ) -> torch.Tensor:
+        """
+        Compute the MaxSim score (ColBERT-like) for the given multi-vector query and passage embeddings.
+        """
+        return self.score_multi_vector(qs, ps, device=device, **kwargs)
