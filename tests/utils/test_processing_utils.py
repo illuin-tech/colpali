4cc8846b16078fd65937ecfcca2aba447c9924cd
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-11 09:53:30 +0200

    feat: rename scorer test to `test_processing_utils`

--- tests/utils/test_processing_utils.py
+++ tests/utils/test_processing_utils.py
@@ -8,19 +8,19 @@ from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
 
 @pytest.fixture
 @patch.multiple(BaseVisualRetrieverProcessor, __abstractmethods__=set())
-def vector_scorer() -> BaseVisualRetrieverProcessor:
-    return BaseVisualRetrieverProcessor()
+def processor() -> BaseVisualRetrieverProcessor:
+    return BaseVisualRetrieverProcessor()  # type: ignore
 
 
-def test_score_single_vector_embeddings(vector_scorer: BaseVisualRetrieverProcessor):
+def test_score_single_vector_embeddings(processor: BaseVisualRetrieverProcessor):
     qs = [torch.randn(128) for _ in range(10)]
     ps = [torch.randn(128) for _ in range(10)]
-    scores = vector_scorer.score_single_vector(qs, ps)
+    scores = processor.score_single_vector(qs, ps)
     assert scores.shape == (len(qs), len(ps))
 
 
-def test_score_multi_vector_embeddings(vector_scorer: BaseVisualRetrieverProcessor):
+def test_score_multi_vector_embeddings(processor: BaseVisualRetrieverProcessor):
     qs = [torch.randn(10, 128) for _ in range(10)]
     ps = [torch.randn(42, 128) for _ in range(10)]
-    scores = vector_scorer.score_multi_vector(qs, ps)
+    scores = processor.score_multi_vector(qs, ps)
     assert scores.shape == (len(qs), len(ps))
