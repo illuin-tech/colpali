c135323db78f3f8f3b39e861bb6ce09d7e30c935
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-12 09:28:44 +0200

    fix: fix example inference script

--- scripts/infer/run_inference_with_python.py
+++ scripts/infer/run_inference_with_python.py
@@ -5,22 +5,33 @@ import typer
 from datasets import Dataset, load_dataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import AutoProcessor
 
 from colpali_engine.models import ColPali
+from colpali_engine.models.paligemma.colpali.processing_colpali import ColPaliProcessor
 from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
+from colpali_engine.utils.torch_utils import get_torch_device
 
 
 def main() -> None:
-    """Example script to run inference with ColPali"""
+    """
+    Example script to run inference with ColPali
+    """
+
+    device = get_torch_device("auto")
+    print(f"Device used: {device}")
+
+    # Define adapter name
+    base_model_name = "vidore/colpaligemma-3b-pt-448-base"
+    adapter_name = "vidore/colpali-v1.2"
 
     # Load model
-    model_name = "vidore/colpali-v1.2"
     model = ColPali.from_pretrained(
-        "vidore/colpaligemma-3b-pt-448-base", torch_dtype=torch.bfloat16, device_map="cuda"
+        base_model_name,
+        torch_dtype=torch.bfloat16,
+        device_map=device,
     ).eval()
-    model.load_adapter(model_name)
-    processor = AutoProcessor.from_pretrained(model_name)
+    model.load_adapter(adapter_name)
+    processor = cast(ColPaliProcessor, ColPaliProcessor.from_pretrained("google/paligemma-3b-mix-448"))
 
     if not isinstance(processor, BaseVisualRetrieverProcessor):
         raise ValueError("Processor should be a BaseVisualRetrieverProcessor")
@@ -28,7 +39,7 @@ def main() -> None:
     images = cast(Dataset, load_dataset("vidore/docvqa_test_subsampled", split="test"))["image"]
     queries = ["From which university does James V. Fiorca come ?", "Who is the japanese prime minister?"]
 
-    # run inference - docs
+    # Run inference - docs
     dataloader = DataLoader(
         images,
         batch_size=4,
@@ -42,7 +53,7 @@ def main() -> None:
             embeddings_doc = model(**batch_doc)
         ds.extend(list(torch.unbind(embeddings_doc.to("cpu"))))
 
-    # run inference - queries
+    # Run inference - queries
     dataloader = DataLoader(
         queries,
         batch_size=4,
