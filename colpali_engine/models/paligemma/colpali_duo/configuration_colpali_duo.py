2c2f337773e19f229937b753906587eee62453b7
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-10-16 15:39:16 +0200

    feat: working ColPaliDuo

--- colpali_engine/models/paligemma/colpali_duo/configuration_colpali_duo.py
+++ colpali_engine/models/paligemma/colpali_duo/configuration_colpali_duo.py
@@ -17,3 +17,17 @@ class ColPaliDuoConfig(PaliGemmaConfig):
         self.single_vector_projector_dim = single_vector_projector_dim
         self.single_vector_pool_strategy = single_vector_pool_strategy
         self.multi_vector_projector_dim = multi_vector_projector_dim
+
+
+def to_vlm_backbone_config(config: ColPaliDuoConfig) -> PaliGemmaConfig:
+    """
+    Returns the configuration of the VLM backbone.
+    """
+    vlm_backbone_config = config.to_dict()
+
+    # Remove the additional attributes
+    vlm_backbone_config.pop("single_vector_projector_dim")
+    vlm_backbone_config.pop("single_vector_pool_strategy")
+    vlm_backbone_config.pop("multi_vector_projector_dim")
+
+    return PaliGemmaConfig(**vlm_backbone_config)
--- colpali_engine/models/paligemma/colpali_duo/publish_base_colpali_duo.py
+++ colpali_engine/models/paligemma/colpali_duo/publish_base_colpali_duo.py
@@ -1,24 +1,38 @@
 from typing import cast
 
+import torch
 import typer
-from transformers.models.paligemma.configuration_paligemma import PaliGemmaConfig
 
+from colpali_engine.models.paligemma.colpali.modeling_colpali import ColPali
 from colpali_engine.models.paligemma.colpali_duo.configuration_colpali_duo import ColPaliDuoConfig
 from colpali_engine.models.paligemma.colpali_duo.modeling_colpali_duo import ColPaliDuo
+from colpali_engine.utils.torch_utils import get_torch_device
 
 
 def main():
-    vlm_backbone_model_name = "google/paligemma-3b-mix-448"
-    base_colpali_duo_name = "vidore/colpali-v2.0-test-0.1"
+    """
+    Publish the base ColPaliDuo model to the hub.
+    """
+    base_colpali_duo_name = "vidore/colpali-duo-base-0.1"
+    device = get_torch_device("auto")
+
+    old_model = cast(
+        ColPali,
+        ColPali.from_pretrained(
+            "vidore/colpali-v1.2",
+            torch_dtype=torch.bfloat16,
+            device_map=device,  # or "mps" if on Apple Silicon
+        ),
+    ).eval()
 
     model_config = ColPaliDuoConfig(
-        vlm_backbone_config=cast(PaliGemmaConfig, PaliGemmaConfig.from_pretrained(vlm_backbone_model_name)),
+        **old_model.config.to_dict(),
         single_vector_projector_dim=1024,
         single_vector_pool_strategy="mean",
         multi_vector_projector_dim=128,
     )
 
-    model = ColPaliDuo(config=model_config)
+    model = ColPaliDuo(config=model_config).to(device).to(torch.bfloat16).eval()
 
     model.push_to_hub(base_colpali_duo_name, private=True)
 
