5a2f15f867f104faedcbda5260f8b38522314637
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-10-16 16:03:50 +0200

    docs: add TODO

--- colpali_engine/models/paligemma/colpali_duo/processing_colpali_duo.py
+++ colpali_engine/models/paligemma/colpali_duo/processing_colpali_duo.py
@@ -4,6 +4,8 @@ from colpali_engine.models.paligemma.colpali.processing_colpali import ColPaliPr
 class ColPaliDuoProcessor(ColPaliProcessor):
     """
     Processor for ColPaliDuo. Mirrors the `ColPaliProcessor` class.
+
+    TODO: duplicate transformers-ready code from https://github.com/huggingface/transformers/pull/33736 once merged
     """
 
     def __init__(self, *args, **kwargs):
