a173f32c6a31e4cdcc4e0c792a31d8c4fc4fbba2
ManuelFay <manuel.faysse@illuin.tech>
2024-10-21 15:10:29 +0200

    rebase and rugg

--- colpali_engine/models/__init__.py
+++ colpali_engine/models/__init__.py
@@ -1,4 +1,13 @@
 from .idefics_2 import BiIdefics2, ColIdefics2, ColIdefics2Processor
-from .paligemma import BiPali, BiPaliProcessor, BiPaliProj, ColPali, ColPaliProcessor
-from .paligemma import  ColPaliDuo, ColPaliDuoConfig, ColPaliDuoModelOutput, ColPaliDuoProcessor
+from .paligemma import (
+    BiPali,
+    BiPaliProcessor,
+    BiPaliProj,
+    ColPali,
+    ColPaliDuo,
+    ColPaliDuoConfig,
+    ColPaliDuoModelOutput,
+    ColPaliDuoProcessor,
+    ColPaliProcessor,
+)
 from .qwen2 import BiQwen2, BiQwen2Processor, ColQwen2, ColQwen2Processor
