e1a1fba1840534f3cfa36af111b09f8860076012
ManuelFay <manuel.faysse@illuin.tech>
2024-10-22 00:15:26 +0200

    hack

--- colpali_engine/models/paligemma/colpali_duo/processing_colpali_duo.py
+++ colpali_engine/models/paligemma/colpali_duo/processing_colpali_duo.py
@@ -1,3 +1,5 @@
+from typing import List, Optional, Union
+import torch
 from colpali_engine.models.paligemma.colpali.processing_colpali import ColPaliProcessor
 
 
@@ -10,3 +12,16 @@ class ColPaliDuoProcessor(ColPaliProcessor):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.mode = "single_vector"
+
+    def score(
+        self,
+        qs: List[torch.Tensor],
+        ps: List[torch.Tensor],
+        device: Optional[Union[str, torch.device]] = None,
+        **kwargs,
+    ) -> torch.Tensor:
+        """
+        Compute the MaxSim score (ColBERT-like) for the given multi-vector query and passage embeddings.
+        """
+        return self.score_single_vector(qs, ps, device=device)
