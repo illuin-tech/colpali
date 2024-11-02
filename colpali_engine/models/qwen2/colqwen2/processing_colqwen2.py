6331990304a239f40a896a324840f80427e32274
ManuelFay <manuel.faysse@illuin.tech>
2024-11-02 18:09:24 +0100

    change padding tokens

--- colpali_engine/models/qwen2/colqwen2/processing_colqwen2.py
+++ colpali_engine/models/qwen2/colqwen2/processing_colqwen2.py
@@ -30,14 +30,13 @@ class ColQwen2Processor(BaseVisualRetrieverProcessor, Qwen2VLProcessor):
     """
 
     visual_prompt_prefix: ClassVar[str] = (
-        "<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe the image.<|im_end|>\n"
+        "<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe the image.<|im_end|><|endoftext|>"
     )
 
     # FIXME: `query_augmentation_token` was set to hardcoded value "<pad>" in the original code used to train
     # "vidore/colqwen2-v0.1", while it should have been set to `processor.tokenizer.pad_token`.
     # TODO: Fix training script for next ColQwen2 release.
-    query_augmentation_token: ClassVar[str] = "<pad>"
-
+    query_augmentation_token: ClassVar[str] = "<|endoftext|>"
     image_token: ClassVar[str] = "<|image_pad|>"
 
     @property
