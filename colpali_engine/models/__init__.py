d291bd6c4b64f215005f47f15d20aef54773b8c9
ManuelFay <manuel.faysse@illuin.tech>
2024-11-26 19:21:26 +0100

    smolvlm

--- colpali_engine/models/__init__.py
+++ colpali_engine/models/__init__.py
@@ -1,3 +1,4 @@
+from .idefics3 import ColIdefics3, ColIdefics3Processor
 from .idefics_2 import BiIdefics2, ColIdefics2, ColIdefics2Processor
 from .paligemma import BiPali, BiPaliProcessor, BiPaliProj, ColPali, ColPaliProcessor
 from .qwen2 import BiQwen2, BiQwen2Processor, ColQwen2, ColQwen2Processor
