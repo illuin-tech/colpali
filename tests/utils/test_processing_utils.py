d99ba2f4836f33215cd06555fbe33cf51a6e566e
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-11-28 20:56:37 +0100

    Fix typing for inputs for `processor.score_multi_vector`  (#132)
    * refactor: allow `torch.Tensor` inputs for `score_multi_vector`
    
    * chore: update CHANGELOG

--- tests/utils/test_processing_utils.py
+++ tests/utils/test_processing_utils.py
@@ -5,6 +5,8 @@ import torch
 
 from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
 
+EMBEDDING_DIM = 32
+
 
 @pytest.fixture
 @patch.multiple(BaseVisualRetrieverProcessor, __abstractmethods__=set())
@@ -13,14 +15,30 @@ def processor() -> BaseVisualRetrieverProcessor:
 
 
 def test_score_single_vector_embeddings(processor: BaseVisualRetrieverProcessor):
-    qs = [torch.randn(128) for _ in range(10)]
-    ps = [torch.randn(128) for _ in range(10)]
+    qs = [torch.randn(EMBEDDING_DIM) for _ in range(4)]
+    ps = [torch.randn(EMBEDDING_DIM) for _ in range(8)]
     scores = processor.score_single_vector(qs, ps)
     assert scores.shape == (len(qs), len(ps))
 
 
 def test_score_multi_vector_embeddings(processor: BaseVisualRetrieverProcessor):
-    qs = [torch.randn(10, 128) for _ in range(10)]
-    ps = [torch.randn(42, 128) for _ in range(10)]
-    scores = processor.score_multi_vector(qs, ps)
-    assert scores.shape == (len(qs), len(ps))
+    # Score from list input
+    qs = [
+        torch.randn(2, EMBEDDING_DIM),
+        torch.randn(4, EMBEDDING_DIM),
+    ]
+    ps = [
+        torch.randn(8, EMBEDDING_DIM),
+        torch.randn(4, EMBEDDING_DIM),
+        torch.randn(16, EMBEDDING_DIM),
+    ]
+    scores_from_list_input = processor.score_multi_vector(qs, ps)
+    assert scores_from_list_input.shape == (len(qs), len(ps))
+
+    # Score from tensor input
+    qs_padded = torch.nn.utils.rnn.pad_sequence(qs, batch_first=True)
+    ps_padded = torch.nn.utils.rnn.pad_sequence(ps, batch_first=True)
+    scores_from_tensor = processor.score_multi_vector(qs_padded, ps_padded)
+    assert scores_from_tensor.shape == (len(qs), len(ps))
+
+    assert torch.allclose(scores_from_list_input, scores_from_tensor), "Scores from list and tensor inputs should match"
