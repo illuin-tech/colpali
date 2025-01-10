0bf8c4dac764ba131ddc9c96001f1968fbdd56f3
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2025-01-10 15:26:38 +0100

    Loosen package dependencies (#166)
    * build: loosen package deps
    
    * test: add expected scores in E2E test for ColPali
    
    * chore: update CHANGELOG
    
    * build: restore transformers dep's upper bound
    
    * build: restore pillow dep's upper bound
    
    * build: remove duplicate dep
    
    * chore: add `0.3.6` release in changelog

--- tests/models/paligemma/colpali/test_colpali_e2e.py
+++ tests/models/paligemma/colpali/test_colpali_e2e.py
@@ -2,7 +2,7 @@ from typing import cast
 
 import pytest
 import torch
-from PIL import Image
+from datasets import load_dataset
 
 from colpali_engine.models import ColPali, ColPaliProcessor
 from colpali_engine.utils.torch_utils import get_torch_device
@@ -15,6 +15,7 @@ def model_name() -> str:
 
 @pytest.mark.slow
 def test_e2e_retrieval_and_scoring(model_name: str):
+    # Load the model and processor
     model = cast(
         ColPali,
         ColPali.from_pretrained(
@@ -23,31 +24,39 @@ def test_e2e_retrieval_and_scoring(model_name: str):
             device_map=get_torch_device("auto"),
         ),
     ).eval()
+    processor = cast(ColPaliProcessor, ColPaliProcessor.from_pretrained(model_name))
 
-    try:
-        processor = cast(ColPaliProcessor, ColPaliProcessor.from_pretrained(model_name))
+    # Load the test dataset
+    ds = load_dataset("hf-internal-testing/document-visual-retrieval-test", split="test")
 
-        # Your inputs
-        images = [
-            Image.new("RGB", (480, 480), color="white"),
-            Image.new("RGB", (250, 250), color="black"),
-        ]
-        queries = [
-            "Is attention really all you need?",
-            "Are Benjamin, Antoine, Merve, and Jo best friends?",
-        ]
+    # Preprocess the examples
+    batch_images = processor.process_images(images=ds["image"]).to(model.device)
+    batch_queries = processor.process_queries(queries=ds["query"]).to(model.device)
 
-        # Process the inputs
-        batch_images = processor.process_images(images).to(model.device)
-        batch_queries = processor.process_queries(queries).to(model.device)
+    # Run inference
+    with torch.inference_mode():
+        image_embeddings = model(**batch_images)
+        query_embeddings = model(**batch_queries)
 
-        # Forward pass
-        with torch.no_grad():
-            image_embeddings = model(**batch_images)
-            query_embeddings = model(**batch_queries)
+    # Compute retrieval scores
+    scores = processor.score_multi_vector(
+        qs=query_embeddings,
+        ps=image_embeddings,
+    )  # (len(qs), len(ps))
 
-        scores = processor.score_multi_vector(query_embeddings, image_embeddings)
-        assert isinstance(scores, torch.Tensor)
+    assert scores.ndim == 2, f"Expected 2D tensor, got {scores.ndim}"
+    assert scores.shape == (len(ds), len(ds)), f"Expected shape {(len(ds), len(ds))}, got {scores.shape}"
 
-    except Exception as e:
-        pytest.fail(f"Code raised an exception: {e}")
+    # Check if the maximum scores per row are in the diagonal of the matrix score
+    assert (scores.argmax(dim=1) == torch.arange(len(ds), device=scores.device)).all()
+
+    # Further validation: fine-grained check, with a hardcoded score from the original implementation
+    expected_scores = torch.tensor(
+        [
+            [16.5000, 7.5938, 15.6875],
+            [12.0625, 16.2500, 11.1250],
+            [15.2500, 12.6250, 21.0000],
+        ],
+        dtype=scores.dtype,
+    )
+    assert torch.allclose(scores, expected_scores, atol=1), f"Expected scores {expected_scores}, got {scores}"
