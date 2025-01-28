9ecf8b91eef0a07eec41d9ac8c53bd249458c5eb
Quentin Mace <quentin.mace@illuin.tech>
2025-01-28 10:24:15 +0100

    Revert "lint fix"
    This reverts commit 7820e3a826a684ffa311132f4bece6a9975f421c.

--- colpali_engine/models/idefics3/colidefics3/publish_base_colidefics3.py
+++ colpali_engine/models/idefics3/colidefics3/publish_base_colidefics3.py
@@ -2,41 +2,47 @@ from typing import Annotated, cast
 
 import torch
 import typer
-from transformers import AutoProcessor
+from transformers.models.idefics3 import Idefics3ForConditionalGeneration
 
 from colpali_engine.models.idefics3.colidefics3.modeling_colidefics3 import ColIdefics3
 from colpali_engine.utils.torch_utils import get_torch_device
 
 
 def main(
-    model_name: Annotated[
-        str, typer.Option(help="The name of the VLM backbone model to use.")
-    ],
-    new_base_model_name: Annotated[
-        str, typer.Option(help="The name of the base model to push to the hub.")
-    ],
+    vlm_backbone_name: Annotated[str, typer.Option(help="The name of the VLM backbone model to use.")],
+    new_base_model_name: Annotated[str, typer.Option(help="The name of the base model to push to the hub.")],
 ):
     """
     Publish the base ColIdefics3 model to the hub.
 
     Args:
-    - model_name (str): The name of the VLM backbone model to use.
+    - vlm_backbone_name (str): The name of the VLM backbone model to use.
     - new_base_model_name (str): The name of the base model to push to the hub.
 
     Example usage:
     ```bash
     python colpali_engine/models/idefics3/colidefics3/publish_base_colidefics3.py \
-        --model-name vidore/SmolVLM-Instruct-500M \
-        --new-base-model-name vidore/MyNew_ColSmolVLM-Instruct-500M
+        --vlm-backbone-name smol-explorers/SmolVLM-256M-Base-25750 \
+        --new-base-model-name vidore/colsmolvlm-256M-base
     ```
     """
     device = get_torch_device("auto")
 
-    model = ColIdefics3.from_pretrained(model_name).to(device).to(torch.bfloat16).eval()
-    processor = AutoProcessor.from_pretrained(model_name)
+    vlm_backbone = cast(
+        Idefics3ForConditionalGeneration,
+        Idefics3ForConditionalGeneration.from_pretrained(
+            vlm_backbone_name,
+            torch_dtype=torch.bfloat16,
+            device_map=device,
+        ),
+    ).eval()
+
+    model = ColIdefics3(config=vlm_backbone.config).to(device).to(torch.bfloat16).eval()
+
+    # Copy pre-trained weights from old model
+    model.load_state_dict(vlm_backbone.state_dict(), strict=False)
 
     model.push_to_hub(new_base_model_name, private=True)
-    processor.push_to_hub(new_base_model_name, private=True)
 
     return
 
