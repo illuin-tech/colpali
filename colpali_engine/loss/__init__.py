2151d53aae70a3d07a1e8718e878952c4a1f61f7
ManuelFay <manuel.faysse@illuin.tech>
2025-02-18 16:05:01 +0100

    fff

--- colpali_engine/loss/__init__.py
+++ colpali_engine/loss/__init__.py
@@ -3,6 +3,13 @@ from .bi_encoder_losses import (
     BiPairwiseCELoss,
     BiPairwiseNegativeCELoss,
 )
+
+from .gradcache_late_interaction_losses import (
+    CachedColbertLoss,
+    CachedColbertPairwiseCELoss,
+    CachedColbertPairwiseNegativeCELoss,
+)
+
 from .late_interaction_losses import (
     ColbertLoss,
     ColbertPairwiseCELoss,
