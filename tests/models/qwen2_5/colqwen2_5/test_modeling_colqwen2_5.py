2f3c8bacc272e91a509f19faeca35ae778d305cf
Quentin Mace <quentin.mace@illuin.tech>
2025-02-19 13:48:25 +0100

    refactor tests

--- tests/models/qwen2_5/colqwen2_5/test_modeling_colqwen2_5.py
+++ tests/models/qwen2_5/colqwen2_5/test_modeling_colqwen2_5.py
@@ -3,7 +3,9 @@ from typing import Generator, cast
 
 import pytest
 import torch
+from datasets import load_dataset
 from PIL import Image
+from transformers.utils.import_utils import is_flash_attn_2_available
 
 from colpali_engine.models import ColQwen2_5, ColQwen2_5_Processor
 from colpali_engine.utils.torch_utils import get_torch_device, tear_down_torch
@@ -13,13 +15,18 @@ logger = logging.getLogger(__name__)
 
 @pytest.fixture(scope="module")
 def model_name() -> str:
-    return "vidore/colqwen2_5-v0.1"
+    return "vidore/colqwen2.5-v0.2"
 
 
 @pytest.fixture(scope="module")
-def model_from_pretrained(model_name: str) -> Generator[ColQwen2_5, None, None]:
+def model(model_name: str) -> Generator[ColQwen2_5, None, None]:
     device = get_torch_device("auto")
     logger.info(f"Device used: {device}")
+
+    if device.startswith("cuda"):
+        attn_implementation = "flash_attention_2" if is_flash_attn_2_available() else None
+    else:
+        attn_implementation = "eager"
 
     yield cast(
         ColQwen2_5,
@@ -27,6 +34,7 @@ def model_from_pretrained(model_name: str) -> Generator[ColQwen2_5, None, None]:
             model_name,
             torch_dtype=torch.bfloat16,
             device_map=device,
+            attn_implementation=attn_implementation,
         ).eval(),
     )
     tear_down_torch()
@@ -37,76 +45,90 @@ def processor(model_name: str) -> Generator[ColQwen2_5_Processor, None, None]:
     yield cast(ColQwen2_5_Processor, ColQwen2_5_Processor.from_pretrained(model_name))
 
 
-@pytest.mark.slow
-def test_load_from_pretrained(model_from_pretrained: ColQwen2_5):
-    assert isinstance(model_from_pretrained, ColQwen2_5)
+class TestColQwen2_5_Model:  # noqa N801
+    @pytest.mark.slow
+    def test_load_model_from_pretrained(self, model: ColQwen2_5):
+        assert isinstance(model, ColQwen2_5)
 
 
-@pytest.mark.slow
-def test_forward_images(
-    model_from_pretrained: ColQwen2_5,
-    processor: ColQwen2_5_Processor,
-):
-    # Create a batch of dummy images
-    images = [
-        Image.new("RGB", (32, 32), color="white"),
-        Image.new("RGB", (16, 16), color="black"),
-    ]
+class TestColQwen2_5_ModelIntegration:  # noqa N801
+    @pytest.mark.slow
+    def test_forward_images_integration(
+        self,
+        model: ColQwen2_5,
+        processor: ColQwen2_5_Processor,
+    ):
+        # Create a batch of dummy images
+        images = [
+            Image.new("RGB", (64, 64), color="white"),
+            Image.new("RGB", (32, 32), color="black"),
+        ]
 
-    # Process the image
-    batch_images = processor.process_images(images).to(model_from_pretrained.device)
+        # Process the image
+        batch_images = processor.process_images(images).to(model.device)
 
-    # Forward pass
-    with torch.no_grad():
-        outputs = model_from_pretrained(**batch_images)
+        # Forward pass
+        with torch.no_grad():
+            outputs = model(**batch_images)
 
-    # Assertions
-    assert isinstance(outputs, torch.Tensor)
-    assert outputs.dim() == 3
-    batch_size, n_visual_tokens, emb_dim = outputs.shape
-    assert batch_size == len(images)
-    assert emb_dim == model_from_pretrained.dim
+        # Assertions
+        assert isinstance(outputs, torch.Tensor)
+        assert outputs.dim() == 3
+        batch_size, n_visual_tokens, emb_dim = outputs.shape
+        assert batch_size == len(images)
+        assert emb_dim == model.dim
 
+    @pytest.mark.slow
+    def test_forward_queries_integration(
+        self,
+        model: ColQwen2_5,
+        processor: ColQwen2_5_Processor,
+    ):
+        queries = [
+            "Is attention really all you need?",
+            "Are Benjamin, Antoine, Merve, and Jo best friends?",
+        ]
 
-@pytest.mark.slow
-def test_forward_queries(model_from_pretrained: ColQwen2_5, processor: ColQwen2_5_Processor):
-    queries = [
-        "Is attention really all you need?",
-        "Are Benjamin, Antoine, Merve, and Jo best friends?",
-    ]
+        # Process the queries
+        batch_queries = processor.process_queries(queries).to(model.device)
 
-    # Process the queries
-    batch_queries = processor.process_queries(queries).to(model_from_pretrained.device)
+        # Forward pass
+        with torch.no_grad():
+            outputs = model(**batch_queries)
 
-    # Forward pass
-    with torch.no_grad():
-        outputs = model_from_pretrained(**batch_queries)
+        # Assertions
+        assert isinstance(outputs, torch.Tensor)
+        assert outputs.dim() == 3
+        batch_size, n_query_tokens, emb_dim = outputs.shape
+        assert batch_size == len(queries)
+        assert emb_dim == model.dim
 
-    # Assertions
-    assert isinstance(outputs, torch.Tensor)
-    assert outputs.dim() == 3
-    batch_size, n_query_tokens, emb_dim = outputs.shape
-    assert batch_size == len(queries)
-    assert emb_dim == model_from_pretrained.dim
+    @pytest.mark.slow
+    def test_retrieval_integration(
+        self,
+        model: ColQwen2_5,
+        processor: ColQwen2_5_Processor,
+    ):
+        # Load the test dataset
+        ds = load_dataset("hf-internal-testing/document-visual-retrieval-test", split="test")
 
+        # Preprocess the examples
+        batch_images = processor.process_images(images=ds["image"]).to(model.device)
+        batch_queries = processor.process_queries(queries=ds["query"]).to(model.device)
 
-@pytest.mark.slow
-def test_is_model_deterministic(model_from_pretrained: ColQwen2_5, processor: ColQwen2_5_Processor):
-    queries = [
-        "Is attention really all you need?",
-        "Are Benjamin, Antoine, Merve, and Jo best friends?",
-    ]
+        # Run inference
+        with torch.inference_mode():
+            image_embeddings = model(**batch_images)
+            query_embeddings = model(**batch_queries)
 
-    # Process the queries
-    batch_queries = processor.process_queries(queries).to(model_from_pretrained.device)
+        # Compute retrieval scores
+        scores = processor.score_multi_vector(
+            qs=query_embeddings,
+            ps=image_embeddings,
+        )  # (len(qs), len(ps))
 
-    # Forward pass
-    with torch.no_grad():
-        outputs = model_from_pretrained(**batch_queries)
+        assert scores.ndim == 2, f"Expected 2D tensor, got {scores.ndim}"
+        assert scores.shape == (len(ds), len(ds)), f"Expected shape {(len(ds), len(ds))}, got {scores.shape}"
 
-    # Forward pass again
-    with torch.no_grad():
-        outputs_new = model_from_pretrained(**batch_queries)
-
-    # Assertions
-    assert torch.allclose(outputs, outputs_new, atol=1e-5)
+        # Check if the maximum scores per row are in the diagonal of the matrix score
+        assert (scores.argmax(dim=1) == torch.arange(len(ds), device=scores.device)).all()
