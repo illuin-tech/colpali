df5a89a84fd4912299544a2bcb997c0b532e5cfe
ManuelFay <manuel.faysse@illuin.tech>
2025-02-21 12:54:42 +0100

    smart resize

--- colpali_engine/trainer/colmodel_training.py
+++ colpali_engine/trainer/colmodel_training.py
@@ -136,6 +136,8 @@ class ColModelTraining:
         # self.dataset = self.dataset.map(lambda x: self.preprocess_example(x, self.config.processor),
         #                                 num_proc=self.config.tr_args.dataloader_num_workers)
 
+        self.dataset = self.dataset.map(lambda x: {"image": self.config.processor.smart_resize(x["image"])}, num_proc=1)
+
 
         trainer = ContrastiveTrainer(
             model=self.model,
