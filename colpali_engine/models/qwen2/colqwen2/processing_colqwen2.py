8b67cd499eeb7cf972fa864cb403a5e20c359078
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2025-02-25 18:04:05 +0100

    refactor: remove duplicate resize code in `ColQwen2Processor`

--- colpali_engine/models/qwen2/colqwen2/processing_colqwen2.py
+++ colpali_engine/models/qwen2/colqwen2/processing_colqwen2.py
@@ -1,27 +1,12 @@
-import math
 from typing import ClassVar, List, Optional, Tuple, Union
 
 import torch
 from PIL import Image
 from transformers import BatchFeature
 from transformers.models.qwen2_vl import Qwen2VLProcessor
+from transformers.models.qwen2_vl.image_processing_qwen2_vl import smart_resize
 
 from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
-
-
-def round_by_factor(number: float, factor: int) -> int:
-    """Returns the closest integer to 'number' that is divisible by 'factor'."""
-    return round(number / factor) * factor
-
-
-def ceil_by_factor(number: float, factor: int) -> int:
-    """Returns the smallest integer greater than or equal to 'number' that is divisible by 'factor'."""
-    return math.ceil(number / factor) * factor
-
-
-def floor_by_factor(number: float, factor: int) -> int:
-    """Returns the largest integer less than or equal to 'number' that is divisible by 'factor'."""
-    return math.floor(number / factor) * factor
 
 
 class ColQwen2Processor(BaseVisualRetrieverProcessor, Qwen2VLProcessor):
@@ -43,62 +28,10 @@ class ColQwen2Processor(BaseVisualRetrieverProcessor, Qwen2VLProcessor):
     def __init__(self, *args, **kwargs):
         num_image_tokens = kwargs.pop("num_image_tokens", 768)
         super().__init__(*args, **kwargs)
+
         self.tokenizer.padding_side = "left"
         self.min_pixels = 4 * 28 * 28
         self.max_pixels = num_image_tokens * 28 * 28
-        self.factor = 28
-        self.max_ratio = 200
-
-    @staticmethod
-    def smart_resize_helper(
-        width: int,
-        height: int,
-        factor: int,
-        max_ratio: int,
-        min_pixels: int,
-        max_pixels: int,
-    ) -> Tuple[int, int]:
-        """
-        Returns the image size so that the following conditions are met:
-        1. Both dimensions (height and width) are divisible by 'factor'.
-        2. The total number of pixels is within the range ['min_pixels', 'max_pixels'].
-        3. The aspect ratio of the image is maintained as closely as possible.
-        """
-
-        if max(height, width) / min(height, width) > max_ratio:
-            raise ValueError(
-                f"absolute aspect ratio must be smaller than {max_ratio}, "
-                f"got {max(height, width) / min(height, width)}"
-            )
-
-        h_bar = max(factor, round_by_factor(height, factor))
-        w_bar = max(factor, round_by_factor(width, factor))
-
-        if h_bar * w_bar > max_pixels:
-            beta = math.sqrt((height * width) / max_pixels)
-            h_bar = floor_by_factor(height / beta, factor)
-            w_bar = floor_by_factor(width / beta, factor)
-        elif h_bar * w_bar < min_pixels:
-            beta = math.sqrt(min_pixels / (height * width))
-            h_bar = ceil_by_factor(height * beta, factor)
-            w_bar = ceil_by_factor(width * beta, factor)
-
-        return h_bar, w_bar
-
-    def smart_resize(self, image: Image.Image) -> Image.Image:
-        """
-        Resize and convert the image to the required format.
-        """
-        image_size = image.size
-        resized_height, resized_width = self.smart_resize_helper(
-            width=image_size[0],
-            height=image_size[1],
-            factor=self.factor,
-            max_ratio=self.max_ratio,
-            min_pixels=self.min_pixels,
-            max_pixels=self.max_pixels,
-        )
-        return image.convert("RGB").resize((resized_width, resized_height))
 
     def process_images(
         self,
@@ -108,12 +41,11 @@ class ColQwen2Processor(BaseVisualRetrieverProcessor, Qwen2VLProcessor):
         Process images for ColQwen2.
         """
         texts_doc = [self.visual_prompt_prefix] * len(images)
-
-        resized_images: List[Image.Image] = [self.smart_resize(image) for image in images]
+        images = [image.convert("RGB") for image in images]
 
         batch_doc = self(
             text=texts_doc,
-            images=resized_images,
+            images=images,
             padding="longest",
             return_tensors="pt",
         )
@@ -126,7 +58,7 @@ class ColQwen2Processor(BaseVisualRetrieverProcessor, Qwen2VLProcessor):
         pixel_values = torch.split(batch_doc["pixel_values"], offsets.tolist())
 
         # pad pixel_values to the same length to be able to make it into a tensor
-        max_length = max([len(pv) for pv in pixel_values])
+        max_length = max(len(pixel_value) for pixel_value in pixel_values)
 
         pixel_values = [
             torch.cat([pv, torch.zeros((max_length - len(pv), pv.shape[1]), dtype=pv.dtype, device=pv.device)])
@@ -186,11 +118,9 @@ class ColQwen2Processor(BaseVisualRetrieverProcessor, Qwen2VLProcessor):
         The `spatial_merge_size` is the number of patches that will be merged spatially. It is stored in
         as a `Qwen2VLForConditionalGeneration` attribute under `model.spatial_merge_size`.
         """
-        height_new, width_new = self.smart_resize_helper(
+        height_new, width_new = smart_resize(
             width=image_size[0],
             height=image_size[1],
-            factor=self.factor,
-            max_ratio=self.max_ratio,
             min_pixels=self.min_pixels,
             max_pixels=self.max_pixels,
         )
