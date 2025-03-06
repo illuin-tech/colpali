39c8eb056338a8348910559022d63c68bc3aaef6
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2025-03-06 21:48:46 +0100

    tests: fix `BaseVisualRetrieverProcessor` tests

--- tests/utils/test_processing_utils.py
+++ tests/utils/test_processing_utils.py
@@ -1,6 +1,3 @@
-from unittest.mock import patch
-
-import pytest
 import torch
 
 from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
@@ -8,20 +5,14 @@ from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
 EMBEDDING_DIM = 32
 
 
-@pytest.fixture
-@patch.multiple(BaseVisualRetrieverProcessor, __abstractmethods__=set())
-def processor() -> BaseVisualRetrieverProcessor:
-    return BaseVisualRetrieverProcessor()  # type: ignore
-
-
-def test_score_single_vector_embeddings(processor: BaseVisualRetrieverProcessor):
+def test_score_single_vector_embeddings():
     qs = [torch.randn(EMBEDDING_DIM) for _ in range(4)]
     ps = [torch.randn(EMBEDDING_DIM) for _ in range(8)]
-    scores = processor.score_single_vector(qs, ps)
+    scores = BaseVisualRetrieverProcessor.score_single_vector(qs, ps)
     assert scores.shape == (len(qs), len(ps))
 
 
-def test_score_multi_vector_embeddings(processor: BaseVisualRetrieverProcessor):
+def test_score_multi_vector_embeddings():
     # Score from list input
     qs = [
         torch.randn(2, EMBEDDING_DIM),
@@ -32,13 +23,13 @@ def test_score_multi_vector_embeddings(processor: BaseVisualRetrieverProcessor):
         torch.randn(4, EMBEDDING_DIM),
         torch.randn(16, EMBEDDING_DIM),
     ]
-    scores_from_list_input = processor.score_multi_vector(qs, ps)
+    scores_from_list_input = BaseVisualRetrieverProcessor.score_multi_vector(qs, ps)
     assert scores_from_list_input.shape == (len(qs), len(ps))
 
     # Score from tensor input
     qs_padded = torch.nn.utils.rnn.pad_sequence(qs, batch_first=True)
     ps_padded = torch.nn.utils.rnn.pad_sequence(ps, batch_first=True)
-    scores_from_tensor = processor.score_multi_vector(qs_padded, ps_padded)
+    scores_from_tensor = BaseVisualRetrieverProcessor.score_multi_vector(qs_padded, ps_padded)
     assert scores_from_tensor.shape == (len(qs), len(ps))
 
     assert torch.allclose(scores_from_list_input, scores_from_tensor), "Scores from list and tensor inputs should match"
