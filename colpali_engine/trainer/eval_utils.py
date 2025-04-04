f7be5b509a8f1d6e9f79a3efac0758a14334df7a
Quentin Mace <quentin.mace@illuin.tech>
2025-04-04 17:05:56 +0200

    refactoring

--- colpali_engine/trainer/eval_utils.py
+++ colpali_engine/trainer/eval_utils.py
@@ -2,11 +2,16 @@
 from __future__ import annotations
 
 import logging
+from typing import Dict, Union
 
-from transformers import TrainerControl, TrainerState, TrainingArguments
+from datasets import Dataset
+from peft import PeftModel
+from transformers import PreTrainedModel, TrainerControl, TrainerState, TrainingArguments
 from transformers.integrations import WandbCallback
 from vidore_benchmark.evaluation.vidore_evaluators import ViDoReEvaluatorBEIR, ViDoReEvaluatorQA
 from vidore_benchmark.retrievers import VisionRetriever
+
+from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
 
 logger = logging.getLogger(__name__)
 
@@ -46,13 +51,17 @@ class BenchmarkEvalCallback(WandbCallback):
         dataset_format: str = "beir",
     ):
         """
-        :param processor: The processor instance (e.g., ColIdefics3Processor) needed for retrieval.
-        :eval_dataset_loader: A dictionary with the test dataset names as keys and functions that load the datasets as
-        values.
-        :param batch_query: Batch size for queries.
-        :param batch_passage: Batch size for passages.
-        :param batch_score: Batch size for scoring.
-        :param run_frequency: Frequency of evaluation ver the evaluation triggers.
+        Callback to evaluate the model on a collection of datasets during training.
+
+        Args:
+            processor: The processor to use for the model.
+            model: The model to evaluate.
+            eval_dataset_loader: A dictionary of dataset loading functions.
+            batch_query: Batch size for query.
+            batch_passage: Batch size for passage.
+            batch_score: Batch size for scoring.
+            run_frequency: Frequency of evaluation in steps.
+            dataset_format: Format of the evaluation dataset, either "beir" or "qa".
         """
         self.processor = processor
         self.model = model
@@ -77,34 +86,19 @@ class BenchmarkEvalCallback(WandbCallback):
             return
 
         print(f"\n=== Running benchmark evaluation at global step {state.global_step} ===")
-        # Set model to evaluation mode.
-        self.model.eval()
-
-        # Create a vision retriever with the current model checkpoint.
-        vision_retriever = VisionRetriever(
-            model=self.model,
-            processor=self.processor,
-        )
 
         # Evaluate on a collection.
         if self.eval_dataset_loader is not None:
             try:
                 metrics_collection = {}
                 for test_name, test_dataset_loading_func in self.eval_dataset_loader.items():
-                    ds_coll = test_dataset_loading_func()
-
-                    # Temporary before we are caplable of detecting ds format
-                    if self.eval_dataset_format == "beir":
-                        vidore_evaluator = ViDoReEvaluatorBEIR(vision_retriever=vision_retriever)
-                    elif self.eval_dataset_format == "qa":
-                        vidore_evaluator = ViDoReEvaluatorQA(vision_retriever=vision_retriever)
-                    else:
-                        raise ValueError(f"Invalid eval dataset format: {self.eval_dataset_format}")
-
-                    metrics = vidore_evaluator.evaluate_dataset(
-                        ds=ds_coll,
-                        batch_query=self.batch_query,
+                    metrics = evaluate_dataset(
+                        model=self.model,
+                        processor=self.processor,
+                        dataset=test_dataset_loading_func(),
+                        format=self.eval_dataset_format,
                         batch_passage=self.batch_passage,
+                        batch_query=self.batch_query,
                         batch_score=self.batch_score,
                     )
                     metrics_collection[test_name] = {k: v for k, v in metrics.items() if k in METRICS_TO_TRACK}
@@ -118,3 +112,40 @@ class BenchmarkEvalCallback(WandbCallback):
         # Set model back to train mode.
         self.model.train()
         return
+
+
+def evaluate_dataset(
+    model: Union[PreTrainedModel, PeftModel],
+    processor: BaseVisualRetrieverProcessor,
+    dataset: Dataset,
+    format: str = "beir",
+    batch_passage: int = 4,
+    batch_query: int = 4,
+    batch_score: int = 4,
+) -> Dict[str, float]:
+    """
+    Evaluate a dataset using the vidore-benchmark library.
+    """
+    model.eval()
+
+    # Create a vision retriever with the current model checkpoint.
+    vision_retriever = VisionRetriever(
+        model=model,
+        processor=processor,
+    )
+
+    if format == "qa":
+        vidore_evaluator = ViDoReEvaluatorQA(vision_retriever)
+    elif format == "beir":
+        vidore_evaluator = ViDoReEvaluatorBEIR(vision_retriever)
+    else:
+        raise ValueError(f"Invalid dataset format: {format}, must be 'qa' or 'beir'")
+
+    metrics = vidore_evaluator.evaluate_dataset(
+        ds=dataset,
+        batch_query=batch_query,
+        batch_passage=batch_passage,
+        batch_score=batch_score,
+    )
+
+    return metrics
