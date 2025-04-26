4cd4aab2b054815a383439eb6018ae7d55965c5f
manuel <manuel.faysse@illuin.tech>
2025-04-26 23:32:53 +0800

    debug

--- colpali_engine/loss/late_interaction_losses.py
+++ colpali_engine/loss/late_interaction_losses.py
@@ -160,7 +160,7 @@ class ColbertPairwiseCELoss(torch.nn.Module):
 
 
         import torch.distributed as dist
-        print(pos_scores[0], neg_scores[0], scores[0])
+        print(f"Scores (0): {pos_scores[0].item(), neg_scores[0].item()}, shapes: {scores.shape}, {pos_scores.shape}, {neg_scores.shape}")
         print(f"Rank: {dist.get_rank()}, Offset: {offset}, acc: {(scores.argmax(dim=1) == torch.arange(scores.shape[0], device=scores.device) + offset).sum().item() / scores.shape[0]},  scores: {scores.argmax(dim=1)}")
 
         return loss
