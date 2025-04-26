f913f2717e5727f05dab6b359ab243afa3c14a12
manuel <manuel.faysse@illuin.tech>
2025-04-27 00:27:48 +0800

    pos aware filtering

--- scripts/configs/qwen2/train_colqwen2_model.py
+++ scripts/configs/qwen2/train_colqwen2_model.py
@@ -12,7 +12,7 @@ from colpali_engine.trainer.colmodel_training import ColModelTraining, ColModelT
 from colpali_engine.utils.dataset_transformation import load_train_set
 
 config = ColModelTrainingConfig(
-    output_dir="./models/colqwen2-cesmoothmax-5e-2604",
+    output_dir="./models/colqwen2-cepos-5e-2604",
     processor=ColQwen2Processor.from_pretrained(
         pretrained_model_name_or_path="./models/base_models/colqwen2-base",
         max_num_visual_tokens=1024,
@@ -26,7 +26,7 @@ config = ColModelTrainingConfig(
     dataset_loading_func=load_train_set,
     eval_dataset_loader=None,
     run_eval=True,
-    loss_func=ColbertLoss(normalize_scores=True, use_smooth_max=True, pos_aware_negative_filtering=True),
+    loss_func=ColbertLoss(normalize_scores=True, use_smooth_max=False, pos_aware_negative_filtering=True),
     # loss_func=ColbertPairwiseCELoss(use_smooth_max=True),
     tr_args=TrainingArguments(
         output_dir=None,
