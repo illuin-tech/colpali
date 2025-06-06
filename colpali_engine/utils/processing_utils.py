5e3963a74be66f1c3e146b55de9ba9475bb49b46
manuel <manuel.faysse@illuin.tech>
2025-06-06 11:18:08 +0200

    remove plaid

--- colpali_engine/utils/processing_utils.py
+++ colpali_engine/utils/processing_utils.py
@@ -1,5 +1,3 @@
-import importlib
-import logging
 from abc import ABC, abstractmethod
 from typing import List, Optional, Tuple, Union
 
@@ -8,15 +6,6 @@ from PIL import Image
 from transformers import BatchEncoding, BatchFeature, ProcessorMixin
 
 from colpali_engine.utils.torch_utils import get_torch_device
-
-try:
-    from fast_plaid import search
-except ImportError:
-    # not blocking, just a warning
-    logging.info(
-        "FastPlaid is not installed. Some functionalities will be unavailable. "
-        "Install it with `pip install fast-plaid` if you want to use it."
-    )
 
 
 class BaseVisualRetrieverProcessor(ABC, ProcessorMixin):
@@ -131,63 +120,6 @@ class BaseVisualRetrieverProcessor(ABC, ProcessorMixin):
 
         scores = scores.to(torch.float32)
         return scores
-
-    @staticmethod
-    def get_topk_plaid(
-        qs: Union[torch.Tensor, List[torch.Tensor]],
-        plaid_index: "search.FastPlaid",
-        k: int = 10,
-        batch_size: int = 128,
-        device: Optional[Union[str, torch.device]] = None,
-    ) -> torch.Tensor:
-        """
-        Experimental: Compute the late-interaction/MaxSim score (ColBERT-like) for the given multi-vector
-        query embeddings (`qs`) and passage embeddings endoded in a plaid index. For ColPali, a passage is the
-        image of a document page.
-        """
-        device = device or get_torch_device("auto")
-
-        if len(qs) == 0:
-            raise ValueError("No queries provided")
-
-        scores_list: List[torch.Tensor] = []
-
-        for i in range(0, len(qs), batch_size):
-            scores_batch = []
-            qs_batch = torch.nn.utils.rnn.pad_sequence(qs[i : i + batch_size], batch_first=True, padding_value=0).to(
-                device
-            )
-            # Use the plaid index to get the top-k scores
-            scores_batch = plaid_index.search(
-                queries_embeddings=qs_batch.to(torch.float32),
-                top_k=k,
-            )
-            scores_list.append(scores_batch)
-
-        return scores_list
-
-    @staticmethod
-    def create_plaid_index(
-        ps: Union[torch.Tensor, List[torch.Tensor]],
-        device: Optional[Union[str, torch.device]] = None,
-    ) -> torch.Tensor:
-        """
-        Experimental: Create a FastPlaid index from the given passage embeddings.
-        Args:
-            ps (`Union[torch.Tensor, List[torch.Tensor]]`): Passage embeddings. Should be a list of tensors,
-                where each tensor is of shape (sequence_length_i, embedding_dim).
-            device (`Optional[Union[str, torch.device]]`, *optional*): Device to use for computation. If not
-                provided, uses `get_torch_device("auto")`.
-        """
-        # assert fast_plaid is installed
-        if not importlib.util.find_spec("fast_plaid"):
-            raise ImportError("FastPlaid is not installed. Please install it with `pip install fast-plaid`.")
-
-        fast_plaid_index = search.FastPlaid(index="index")
-        # torch.nn.utils.rnn.pad_sequence(ds, batch_first=True, padding_value=0).to(device)
-        device = device or get_torch_device("auto")
-        fast_plaid_index.create(documents_embeddings=[d.to(device).to(torch.float32) for d in ps])
-        return fast_plaid_index
 
     @abstractmethod
     def get_n_patches(
