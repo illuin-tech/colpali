ef493d8093b71ba7d98f4f54af20b8c949574745
Paul Teiletche <paul.teiletche@gmail.com>
2025-06-12 12:02:57 +0200

    add biidefics3

---
+++ colpali_engine/models/idefics3/biidefics3/processing_biidefics3.py
@@ -0,0 +1,52 @@
+from typing import ClassVar, List, Optional, Tuple, Union
+
+import torch
+from PIL import Image
+from transformers import BatchFeature, BatchEncoding
+
+from colpali_engine.models.idefics3.colidefics3 import ColIdefics3Processor
+
+
+class BiIdefics3Processor(ColIdefics3Processor):  # noqa: N801
+    """
+    Processor for BiIdefics3.
+    """
+
+    def process_texts(
+        self,
+        texts: List[str],
+        max_length: int = 50,
+        contexts: Optional[List[str]] = None,
+        suffix: Optional[str] = None,
+    ) -> Union[BatchFeature, BatchEncoding]:
+        """
+        Process texts for BiIdefics3.
+
+        NOTE: `max_length` is not used and kept only for trainer compatibility.
+        """
+        if suffix is None:
+            suffix = self.query_augmentation_token * 10
+        if contexts is None:
+            contexts = [self.query_prefix] * len(texts)
+
+        prompts = [context + text + suffix for context, text in zip(contexts, texts)]
+
+        batch_texts = self(
+            text=prompts,
+            return_tensors="pt",
+            padding="longest",
+        )
+
+        return batch_texts
+
+    def score(
+        self,
+        qs: List[torch.Tensor],
+        ps: List[torch.Tensor],
+        device: Optional[Union[str, torch.device]] = None,
+        **kwargs,
+    ) -> torch.Tensor:
+        """
+        Compute the cosine similarity for the given query and passage embeddings.
+        """
+        return self.score_single_vector(qs, ps, device=device)
