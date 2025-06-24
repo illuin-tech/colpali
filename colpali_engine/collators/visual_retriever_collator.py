f4e86f8519ae18f6fcba53cc159e9b2a50b66c82
Paul Teiletche <paul.teiletche@gmail.com>
2025-06-24 18:29:07 +0200

    raise error for mixed type batch

--- colpali_engine/collators/visual_retriever_collator.py
+++ colpali_engine/collators/visual_retriever_collator.py
@@ -93,12 +93,17 @@ class VisualRetrieverCollator:
     def auto_collate(self, batch: List[Union[str, Image]], key_prefix: str = "") -> Dict[str, Any]:
         """Automatically collate a batch of documents."""
         # Convert Document objects to their underlying data.
-        if isinstance(batch[0], str):
+        # if type is mixed across the batch, raise an error.
+        all_types = set(type(item) for item in batch)
+        if len(all_types) > 1:
+            raise ValueError(f"Batch contains mixed types: {all_types}. Expected all items to be of the same type.")
+        batch_type = all_types.pop()
+        if isinstance(batch_type, str):
             proc_batch = self.processor.process_texts(
                 texts=batch,
                 max_length=self.max_length,
             )
-        elif isinstance(batch[0], Image):
+        elif isinstance(batch_type, Image):
             proc_batch = self.processor.process_images(
                 images=batch,
             )
