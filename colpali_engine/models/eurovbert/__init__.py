1b8510f11c1192c6a504217d009ad64b7443ca0d
QuentinJGMace <quentinjg.mace@gmail.com>
2025-09-29 11:31:48 +0200

    f

--- colpali_engine/models/eurovbert/__init__.py
+++ colpali_engine/models/eurovbert/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 from .bivbert import BiEuroVBert, BiEuroVBertProcessor
-=======
-from .bivbert import BiVBert as BiEuroVBert
-from .bivbert import BiVBertProcessor as BiEuroVBertProcessor
->>>>>>> 2c87c8a (vbert processor fixes)
 from .colvbert import ColEuroVBert, ColEuroVBertProcessor
