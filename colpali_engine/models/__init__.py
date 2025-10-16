4098a129bfc108f21fc84c2cc2ba74c719f0e9c1
QuentinJGMace <95310069+QuentinJGMace@users.noreply.github.com>
2025-10-16 23:19:15 +0200

    Update colpali_engine/models/__init__.py

--- colpali_engine/models/__init__.py
+++ colpali_engine/models/__init__.py
@@ -3,4 +3,3 @@ from .paligemma import BiPali, BiPaliProcessor, BiPaliProj, ColPali, ColPaliProc
 from .qwen2 import BiQwen2, BiQwen2Processor, ColQwen2, ColQwen2Processor
 from .qwen2_5 import BiQwen2_5, BiQwen2_5_Processor, ColQwen2_5, ColQwen2_5_Processor
 from .modernvbert import BiModernVBert, BiModernVBertProcessor, ColModernVBert, ColModernVBertProcessor
-from .siglip import BiSiglip, BiSiglipProcessor
